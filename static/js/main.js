bead90f439b436bf1c243f0a35e3c2438e1979b0
Salam Chaudhary <chaudharysm00@gmail.com>
2022-11-24 10:19:11 -0800

    modified

--- static/js/main.js
+++ static/js/main.js
@@ -4,9 +4,9 @@ let toggle = document.querySelector('.toggle');
 let topbar = document.querySelector('.topbar');
 let navigation = document.querySelector('.navigation')
 toggle.onclick = function(){
-    toggle.classList.toggle('active');
-    topbar.classList.toggle('active');
-    navigation.classList.toggle('active');
+  toggle.classList.toggle('active');
+  topbar.classList.toggle('active');
+  navigation.classList.toggle('active');
 }
 
 //-----end js for toggle-------///
